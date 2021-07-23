097e4b27fe59567c3484c67b34c377efb614afca
Amaan Khan <amaanite@gmail.com>
2021-07-23 16:12:24 -0500

    Made comments to understand code and data structures

--- src/extract_poseflow.py
+++ src/extract_poseflow.py
@@ -20,14 +20,17 @@ def read_pose(kp_file):
 # TODO: THE ACTUAL CALCULATION BASED ON PREVIOUS FRAME AND CURRENT FRAME
 def calc_pose_flow(prev, next):
     result = np.zeros_like(prev)
-    for kpi in range(prev.shape[0]):
+    for kpi in range(prev.shape[0]): # iterate thru the different landmarks --> shape[0] = # landmarks
+        # kpi = keypoint index
+        # if either frame's landmark value is just (x, y) = (0, 0)
         if np.count_nonzero(prev[kpi]) == 0 or np.count_nonzero(next[kpi]) == 0:
+            # then just assign both to 0 and continue
             result[kpi, 0] = 0.0
             result[kpi, 1] = 0.0
             continue
-
+        # atan2() - https://en.wikipedia.org/wiki/Atan2 --> arctan but has values for [0,2pi] instead of just [-pi,pi]
         ang = math.atan2(next[kpi, 1] - prev[kpi, 1], next[kpi, 0] - prev[kpi, 0])
-        mag = np.linalg.norm(next[kpi] - prev[kpi])
+        mag = np.linalg.norm(next[kpi] - prev[kpi])  # magnitude of the diff of the 2 vectors
 
         result[kpi, 0] = ang
         result[kpi, 1] = mag
@@ -78,27 +81,28 @@ def normalize(poses):
     return poses
 
 
-def main(args):
-    input_dirs = sorted(glob.glob(os.path.join(args.input_dir, '*', '*_color.kp')))
+def main(input_dir):
+    input_dirs = sorted(glob.glob(os.path.join(input_dir, '*', '*_color.kp')))
     input_dir_index = 0
-    total = len(input_dirs)
+    total = len(input_dirs)  # number of videos
 
-    for input_dir in input_dirs:
+    for input_dir in input_dirs:  # iterate thru each folder, which represents each video
         print(f'{input_dir_index}/{total}')
         input_dir_index += 1
 
         output_dir = input_dir.replace('kp', 'kpflow2')
         os.makedirs(output_dir, exist_ok=True)
 
+        # get the individual keypoint files from this video --> i.e, each file represents one frame
         kp_files = sorted(glob.glob(os.path.join(input_dir, '*.json')))
 
-        # 1. Collect all keypoint files and pre-process them
+        # 1. Collect all keypoint files from dir and pre-process them
         poses = []
-        for i in range(len(kp_files)):
+        for i in range(len(kp_files)):  # iterate thru all kp files
             poses.append(read_pose(kp_files[i]))
         poses = np.stack(poses)
-        poses = impute_missing_keypoints(poses)
-        poses = normalize(poses)
+        poses = impute_missing_keypoints(poses) # REVIEW THIS
+        poses = normalize(poses)                # REVIEW THIS
 
         # import matplotlib.pyplot as plt
         # for i in range(poses.shape[0]):
@@ -110,14 +114,15 @@ def main(args):
         # 2. Compute pose flow on pre-processed keypoints
         prev = poses[0]
         for i in range(1, poses.shape[0]):
-            next = poses[i]
-            flow = calc_pose_flow(prev, next)
+            current_frame = poses[i]
+            flow = calc_pose_flow(prev, current_frame)
             np.save(os.path.join(output_dir, 'flow_{:05d}'.format(i - 1)), flow)
-            prev = next
+            prev = current_frame
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--input_dir', type=str)
-    args = parser.parse_args()
-    main(args)
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument('--input_dir', type=str)
+    # args = parser.parse_args()
+    INPUT_DIR = r'C:\Users\amaan\GitHub\ChaLearn-2021-LAP\data\kp'
+    main(INPUT_DIR)
